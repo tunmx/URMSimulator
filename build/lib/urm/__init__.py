32b57646fe98b0ff92fe0a0b002b4f15ca471482
tunm <tunmxy@163.com>
2024-09-27 15:39:47 +0800

    update

---
+++ build/lib/urm/__init__.py
@@ -0,0 +1,5 @@
+from .urm_simulation import C, J, Z, S
+from .urm_simulation import size, haddr, normalize, concat, reloc, allocate, forward, cost
+from .urm_simulation import Instructions, Registers, URMSimulator
+
+
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='urm',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(include=['urm', 'urm.*']),
     description='Unlimited Register Machine (URM) Simulator',
     long_description=open('README.md').read(),
